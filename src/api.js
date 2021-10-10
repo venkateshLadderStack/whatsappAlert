6a6f06ccfe08281047e10697c627ef7970baa74e
venkateshLadderStack <venkatesh@ladderstack.com>
2021-10-10 14:36:54 +0530

    pan

--- src/api.js
+++ src/api.js
@@ -24,7 +24,7 @@ router.post("/", async (req, res) => {
     .create({
       body: req.body,
       from: "whatsapp:+14155238886",
-      to: `whatsapp:+917731012637`,
+      to: `whatsapp:+917287073461`,
     })
     .then((message) => res.send(message.sid))
     .done();
