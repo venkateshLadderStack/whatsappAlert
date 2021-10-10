38244dd092e4fb32477dd8ddd5583da8a094bc46
venkateshLadderStack <venkatesh@ladderstack.com>
2021-10-10 12:22:53 +0530

    started

--- src/api.js
+++ src/api.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const accountSid = process.env.ID;
+const authToken = process.env.API_KEY;
+const client = require("twilio")(accountSid, authToken);
 
 const app = express();
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.json({
-    hello: "hi!"
+    hello: "hi!",
   });
+});
+
+router.post("/", async (req, res) => {
+  await client.messages
+    .create({
+      body: req.body,
+      from: "whatsapp:+14155238886",
+      to: `whatsapp:+917731012637`,
+    })
+    .then((message) => console.log(message.sid))
+    .done();
+
+  res.send(req.body);
 });
 
 app.use(`/.netlify/functions/api`, router);
